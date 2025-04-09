b6591ddb17de7b2670816dcddf82c7e97fe66fee
Vinicius Fernandes Fogaça <viniciusfernandes38@gmail.com>
2025-04-09 16:03:20 -0300

    Ajustando icone

--- src/app/dashboard/simulacao/page.tsx
+++ src/app/dashboard/simulacao/page.tsx
@@ -7,6 +7,7 @@ import { supabase } from '@/lib/supabaseCliente';
 import { formatarMoeda } from '@/utils/moeda';
 import { BarcodeScannerModal } from '@/components/BarcodeScannerModal';
 import PlaceholderImage from '../../../../components/PlaceholderImage';
+import PrintIcon from '../../../../components/PrintIcon';
 
 interface Product {
   id: string;
@@ -340,20 +341,7 @@ export default function SimulacaoPage() {
             window.open('/dashboard/orcamento/impressao', '_blank');
           }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-            />
-          </svg>
+          <PrintIcon />
         </button>
       </>
     )}
